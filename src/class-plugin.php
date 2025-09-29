387ace3cf1f4d54409155638f240700e651a9d6a
Neil Williams <neil@tailormade.uk>
2025-09-29 11:57:50 +0100

    ES Colour Pairings initial commit from plugin starter files

---
+++ src/class-plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Starter main class.
+ *
+ * This class initializes the plugin and contains all
+ * the main hooks and functionality.
+ *
+ * @package PluginStarter
+ */
+
+namespace PluginStarter;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Main Plugin class.
+ */
+class Plugin {
+
+	/**
+	 * Initialize the plugin.
+	 *
+	 * Hooks all functionality into WordPress.
+	 *
+	 * @return void
+	 */
+	public static function init() {
+		// Plugin bootstrap code.
+	}
+}
